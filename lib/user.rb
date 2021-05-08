c5848df351fdf45da29fc1ad70bffb2de7528c16
Mike Work <michael.kerpelman@gmail.com>
2021-05-08 19:50:35 -0400

    merge code creation into single #generate_code, called by #generate_master_code and #make_guess

--- lib/user.rb
+++ lib/user.rb
@@ -4,16 +4,26 @@ class User
 
     include Playable
 
-    attr_reader :master_code
+    attr_reader :master_code, :current_guess
 
     def generate_master_code
-        print "Please enter your 4-digit master code with digits between 1 and 6: "
+        @master_code = generate_code("master code")
+    end
+
+    def make_guess
+        @current_guess = generate_code("guess")
+    end
+
+    private
+
+    def generate_code(code_type)
+        print "Please enter your 4-digit #{code_type} with digits between 1 and 6: "
         code = gets.chomp.chars.map! { |string| string.to_i }
         until verify_code_validity(code)
             print "That's not a valid code. Please try again: "
             code = gets.chomp.chars.map! { |string| string.to_i }
         end
-        @master_code = code
+        code
     end
 
 end

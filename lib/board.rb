3bf21b7d88d13fe23967c301df02b6832222fe1e
Mike Work <michael.kerpelman@gmail.com>
2021-05-08 20:20:47 -0400

    define and test #display

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,9 @@
 require './lib/computer.rb'
-require './lib/player.rb'
+require './lib/user.rb'
 
 class Board
+
+    TOTAL_TURNS = 12
 
     def initialize
         @current_code_values = Array.new(12) { Array.new(4, " ") }
@@ -9,6 +11,17 @@ class Board
     end
 
     def display
-        puts <<-HEREDOC
+        puts "********************  MASTERMIND  *********************"
+        for turn in (1..TOTAL_TURNS)
+            if turn < 10
+                puts "*TURN 0#{turn}* || #{@current_code_values[turn - 1][0]} | #{@current_code_values[turn - 1][1]} | #{@current_code_values[turn - 1][2]} | #{@current_code_values[turn - 1][3]} || Exact: #{@current_matches[turn - 1][0]} | Approximate: #{@current_matches[turn - 1][1]}"
+            else
+                puts "*TURN #{turn}* || #{@current_code_values[turn - 1][0]} | #{@current_code_values[turn - 1][1]} | #{@current_code_values[turn - 1][2]} | #{@current_code_values[turn - 1][3]} || Exact: #{@current_matches[turn - 1][0]} | Approximate: #{@current_matches[turn - 1][1]}"
+            end
+        end
+    end
 
 end
+
+x = Board.new
+x.display
